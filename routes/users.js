de8ec311c77bdc5037c5ac1b86eb9711c076e157
Livia T <livia.trajano05@gmail.com>
2023-07-25 09:03:15 -0300

    v 1.0.2

--- routes/users.js
+++ routes/users.js
@@ -10,4 +10,8 @@ router.get('/cool', function(req, res, next) {
   res.send(`I'm so cool, you're not`);
 });
 
+router.get('/cool/hot', function(req, res, next) {
+  res.send(`I'm so hot, it's better than being cool`);
+});
+
 module.exports = router;
