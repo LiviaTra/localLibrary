4a757c3eab760005e6d87bd645a1ba54ab9f6b31
Livia T <livia.trajano05@gmail.com>
2023-08-15 08:24:30 -0300

    v.0.5 routes and controllers added

--- app.js
+++ app.js
@@ -6,8 +6,19 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const catalogRouter = require('./routes/catalog'); //Import routes for "catalog" area of site
+
+const mongoose = require("mongoose");
 
 var app = express();
+
+mongoose.set("strictQuery", false);
+const mongoDB = "mongodb+srv://locallibrary:1SPETruOL78697hr@cluster0.cs3stsn.mongodb.net/?retryWrites=true&w=majority";
+
+main().catch((err) => console.log(err));
+async function main() {
+  await mongoose.connect(mongoDB);
+}
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
--- routes/users.js
+++ routes/users.js
@@ -6,12 +6,20 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/ifc', function(req, res, next) {
+  res.send(`Bem-vindo ao IFC`);
+});
+
 router.get('/cool', function(req, res, next) {
   res.send(`I'm so cool, you're not`);
 });
 
 router.get('/cool/hot', function(req, res, next) {
   res.send(`I'm so hot, it's better than being cool`);
+});
+
+router.get('/cool/beach', function(req, res, next) {
+  res.send(`I love beaches`);
 });
 
 module.exports = router;
